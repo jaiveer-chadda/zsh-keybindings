cbb7ed8859d02e9ae5884c4df93c1c6a0553c4f5
JV <jaiveer.chadda@gmail.com>
2026-05-31 02:25:22 +0400

    Improved the sudo command so it works with an empty line

--- custom/sudo.zsh
+++ custom/sudo.zsh
@@ -1,10 +1,14 @@
 #!/usr/bin/env zsh
 
-# With thanks to `LindsayHill` and `rothgar`
+# With thanks to `rothgar` and `LindsayHill`
 # https://github.com/rothgar/mastering-zsh/blob/master/docs/helpers/widgets.md
 
 function prepend-sudo() {
-  if [[ "$BUFFER" != 'sudo '* ]] {
+  if ! (( ${#BUFFER//[[:space:]]} )) {
+    BUFFER="sudo $history[$(( HISTCMD - 1 ))]"
+    CURSOR=$#BUFFER
+
+  } elif [[ "$BUFFER" != 'sudo '* ]] {
     BUFFER="sudo $BUFFER"
     CURSOR+=5
   }
