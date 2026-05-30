d25ddff96a8edf9c13c274f2b645e58f09ee3c95
JV <jaiveer.chadda@gmail.com>
2026-05-30 20:03:45 +0400

    Added a way to delete either the whole line, or just everything to the beginning of the line

--- keys/keys.zsh
+++ keys/keys.zsh
@@ -24,11 +24,13 @@ bindkey '^[[1;5D'   backward-word
 
 bindkey '^[[3;5~'   kill-word
 bindkey '^[w'       kill-region
+bindkey '^U'        kill-whole-line     # mapped to `⌘ ⌥ ⌫`
+bindkey '^[^U'      backward-kill-line  # mapped to `⌘   ⌫`
 
 # —— Undo/Redo —————————————————————————————————————————————————————————————— #
 
-bindkey '^_'        undo  # mapped to `⌘  Z`
-bindkey '^[^[^?'    redo  # mapped to `⌘⇧ Z`
+bindkey '^_'        undo  # mapped to `⌘   Z`
+bindkey '^[^[^?'    redo  # mapped to `⌘ ⇧ Z`
 
 # —— Completion ————————————————————————————————————————————————————————————— #
 
