367e99fcc0811f1d8848442750fb3042142c8ddc
JV <jaiveer.chadda@gmail.com>
2026-05-30 19:54:37 +0400

    Organised keybindings

--- keys/keys.zsh
+++ keys/keys.zsh
@@ -1,6 +1,11 @@
 #!/usr/bin/env zsh
 
+# —— Meta ——————————————————————————————————————————————————————————————————— #
+
 bindkey -R '!'-'~'  self-insert
+bindkey ' '         magic-space
+
+# —— Up/Down Navigation ————————————————————————————————————————————————————— #
 
 bindkey '^[[A'        up-line-or-search     # ↑
 bindkey '^[[B'      down-line-or-search     # ↓
@@ -10,14 +15,28 @@ bindkey '^[OB'      down-line-or-search     # ⎋ ↓
 bindkey '^[[5~'       up-line-or-history    # ⇞
 bindkey '^[[6~'     down-line-or-history    # ⇟
 
+# —— Left/Right Navigation —————————————————————————————————————————————————— #
+
 bindkey '^[[1;5C'    forward-word
 bindkey '^[[1;5D'   backward-word
 
+# —— Deleting ——————————————————————————————————————————————————————————————— #
+
 bindkey '^[[3;5~'   kill-word
+bindkey '^[w'       kill-region
+
+# —— Completion ————————————————————————————————————————————————————————————— #
+
 bindkey '^[[Z'      reverse-menu-complete
+
+# —— Editing ———————————————————————————————————————————————————————————————— #
+
 bindkey '^[m'       copy-prev-shell-word
 
-bindkey '^[w'       kill-region
-bindkey ' '         magic-space
+# —— Macros ————————————————————————————————————————————————————————————————— #
 
-bindkey -s "^[l"    "^Q ls^J"
+bindkey -s '^[l'    '^Q ls^J'
+
+# —— Other —————————————————————————————————————————————————————————————————— #
+
+# ——————————————————————————————————————————————————————————————————————————— #
