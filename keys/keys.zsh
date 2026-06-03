ed9bd4aa506a4f2fb493bfa00e9c6ec765ae92a2
JV <jaiveer.chadda@gmail.com>
2026-06-04 01:15:34 +0400

    Updated up/down navigation to make sure it works every time

--- keys/keys.zsh
+++ keys/keys.zsh
@@ -2,55 +2,59 @@
 
 # —— Meta ——————————————————————————————————————————————————————————————————— #
 
-bindkey -R '!'-'~'  self-insert
-bindkey ' '         magic-space
+bindkey -R '!-~'  self-insert
+bindkey    ' '    magic-space
 
 # —— Up/Down Navigation ————————————————————————————————————————————————————— #
 
-autoload -Uz          up-line-or-beginning-search
-autoload -Uz        down-line-or-beginning-search
+autoload -Uz {up,down}-line-or-beginning-search
 
-bindkey '^[[A'        up-line-or-beginning-search      # ↑
-bindkey '^[[B'      down-line-or-beginning-search      # ↓
+zle -N              up-line-or-beginning-search
+zle -N            down-line-or-beginning-search
 
-bindkey '^[OA'        up-line-or-history               # ⎋ ↑
-bindkey '^[OB'      down-line-or-history               # ⎋ ↓
+bindkey '^[[A'      up-line-or-beginning-search      # ↑
+bindkey '^[[B'    down-line-or-beginning-search      # ↓
 
-bindkey '^[[5~'     history-beginning-search-backward  # ⇞
-bindkey '^[[6~'     history-beginning-search-forward   # ⇟
+# ————————————————————————————————————————————— #
+
+bindkey '^[OA'      up-line-or-history               # ⎋ ↑
+bindkey '^[OB'    down-line-or-history               # ⎋ ↓
+
+bindkey '^[[5~'   history-beginning-search-backward  # ⇞
+bindkey '^[[6~'   history-beginning-search-forward   # ⇟
 
 # —— Left/Right Navigation —————————————————————————————————————————————————— #
 
-bindkey '^[[1;5C'    forward-word
-bindkey '^[[1;5D'   backward-word
+bindkey '^[[1;5C'  forward-word
+bindkey '^[[1;5D' backward-word
 
 # —— Editing ———————————————————————————————————————————————————————————————— #
 
-bindkey '^X^O'      overwrite-mode    # mapped to `⌥ o`
-bindkey '^@'        set-mark-command  # mapped to `⌃ ⇥`
-bindkey '^[m'       copy-prev-shell-word
+bindkey '^X^O'    overwrite-mode    # mapped to `⌥ o`
+bindkey '^@'      set-mark-command  # mapped to `⌃ ⇥`
+bindkey '^[m'     copy-prev-shell-word
 
 # —— Deleting ——————————————————————————————————————————————————————————————— #
 
-bindkey '^[[3;5~'   kill-word
-bindkey '^[w'       kill-region
-bindkey '^[^?'      kill-region
-bindkey '^U'        kill-whole-line     # mapped to `⌘ ⌥ ⌫`
-bindkey '^[^U'      backward-kill-line  # mapped to `⌘   ⌫`
+bindkey '^[[3;5~' kill-word
+bindkey '^[w'     kill-region
+bindkey '^[^?'    kill-region
+bindkey '^U'      kill-whole-line     # mapped to `⌘ ⌥ ⌫`
+bindkey '^[^U'    backward-kill-line  # mapped to `⌘   ⌫`
 
 # —— Undo/Redo —————————————————————————————————————————————————————————————— #
 
-bindkey '^_'        undo  # mapped to `⌘   Z`
-bindkey '^[^[^?'    redo  # mapped to `⌘ ⇧ Z`
+bindkey '^_'      undo  # mapped to `⌘   Z`
+bindkey '^[^[^?'  redo  # mapped to `⌘ ⇧ Z`
 
 # —— Completion ————————————————————————————————————————————————————————————— #
 
-bindkey '^[[Z'      reverse-menu-complete
+bindkey '^[[Z'    reverse-menu-complete
 
 # —— Macros ————————————————————————————————————————————————————————————————— #
 
 zle -N lsjv
-bindkey -s '^[l'    '^Qcl^J'
+bindkey -s '^[l'  '^Qcl^J'
 
 # —— Other —————————————————————————————————————————————————————————————————— #
 
