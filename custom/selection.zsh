d5680c57f385e6c5b26ab9e6ad7f23580b8727e5
JV <jaiveer.chadda@gmail.com>
2026-06-23 15:09:23 +0530

    Created `cancel-char-left/right`-like functions for `word` and `line`
    Also standardised the names over all the select/cancel region functions

--- custom/selection.zsh
+++ custom/selection.zsh
@@ -6,34 +6,50 @@ function _set_mark() { if ! (( REGION_ACTIVE )) zle set-mark-command; }
 
 # —— Select Left/Right —————————————————————————————————————————————————————— #
 
-function select-left-char  () { _set_mark; zle     backward-char; }
-function select-right-char () { _set_mark; zle      forward-char; }
-function select-left-word  () { _set_mark; zle     backward-word; }
-function select-right-word () { _set_mark; zle      forward-word; }
-function select-left-line  () { _set_mark; zle beginning-of-line; }
-function select-right-line () { _set_mark; zle       end-of-line; }
+function select-char-left  () { _set_mark; zle     backward-char; }
+function select-char-right () { _set_mark; zle      forward-char; }
 
-zle -N select-left-char ; zle -N select-left-word ; zle -N select-left-line
-zle -N select-right-char; zle -N select-right-word; zle -N select-right-line
+function select-word-left  () { _set_mark; zle     backward-word; }
+function select-word-right () { _set_mark; zle      forward-word; }
 
-bindkey '^[[1;2D'  select-left-char   #   ⇧ <-
-bindkey '^[[1;2C'  select-right-char  #   ⇧ ->
+function select-line-left  () { _set_mark; zle beginning-of-line; }
+function select-line-right () { _set_mark; zle       end-of-line; }
 
-bindkey '^[[1;4D'  select-left-word   # ⌥ ⇧ <-
-bindkey '^[[1;4C'  select-right-word  # ⌥ ⇧ ->
+zle -N select-char-left ; zle -N select-word-left ; zle -N select-line-left
+zle -N select-char-right; zle -N select-word-right; zle -N select-line-right
 
-bindkey '^[[jC;SD' select-left-line   # ⌘ ⇧ <-  [j = jv, C=⌘, S=⇧, D/C = ←/→]
-bindkey '^[[jC;SC' select-right-line  # ⌘ ⇧ ->
+bindkey '^[[1;2D'  select-char-left   #   ⇧ <-
+bindkey '^[[1;2C'  select-char-right  #   ⇧ ->
+
+bindkey '^[[1;4D'  select-word-left   # ⌥ ⇧ <-
+bindkey '^[[1;4C'  select-word-right  # ⌥ ⇧ ->
+
+bindkey '^[[jC;SD' select-line-left   # ⌘ ⇧ <-
+bindkey '^[[jC;SC' select-line-right  # ⌘ ⇧ ->
 
 # —— Cancel Region —————————————————————————————————————————————————————————— #
 
-function cancel-region-left  () { REGION_ACTIVE=0; zle backward-char; }
-function cancel-region-right () { REGION_ACTIVE=0; zle  forward-char; }
+function cancel-char-left  () { REGION_ACTIVE=0; zle     backward-char; }
+function cancel-char-right () { REGION_ACTIVE=0; zle      forward-char; }
 
-zle -N cancel-region-left; zle -N cancel-region-right
+function cancel-word-left  () { REGION_ACTIVE=0; zle     backward-word; }
+function cancel-word-right () { REGION_ACTIVE=0; zle      forward-word; }
 
-bindkey '^[^[[D' cancel-region-left   # ⎋ <-
-bindkey '^[^[[C' cancel-region-right  # ⎋ ->
+function cancel-line-left  () { REGION_ACTIVE=0; zle beginning-of-line; }
+function cancel-line-right () { REGION_ACTIVE=0; zle       end-of-line; }
+
+zle -N cancel-char-left; zle -N cancel-char-right
+zle -N cancel-word-left; zle -N cancel-word-right
+zle -N cancel-line-left; zle -N cancel-line-right
+
+bindkey '^[^[[D' cancel-char-left   # ⎋,   <-
+bindkey '^[^[[C' cancel-char-right  # ⎋,   ->
+
+bindkey '^[^[b'  cancel-word-left   # ⎋, ⌥ <-
+bindkey '^[^[f'  cancel-word-right  # ⎋, ⌥ ->
+
+bindkey '^[^A'   cancel-line-left   # ⎋, ⌘ <-
+bindkey '^[^E'   cancel-line-right  # ⎋, ⌘ ->
 
 # —— Copy Region ———————————————————————————————————————————————————————————— #
 
