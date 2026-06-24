6d76ca715586ae661382cfb167ff558162f496f8
JV <jaiveer.chadda@gmail.com>
2026-06-24 17:15:20 +0530

    Completely reworked (pretty much replaced) the two copying functions.
    This is a lot safer, and every time something happens, I actually have some visual feedback on what happened

--- custom/copy-last-cmd.zsh
+++ custom/copy-last-cmd.zsh
@@ -2,8 +2,70 @@
 
 zmodload -i zsh/parameter
 
-copy-output   () { echo -nE "$( eval $history[$(( HISTCMD-1 ))] )" | pbcopy; }
-copy-last-cmd () { echo -nE         "$history[$(( HISTCMD-1 ))]"   | pbcopy; }
+# ——————————————————————————————————————————————————————————————————————————— #
 
-zle -N copy-output   ; bindkey '^[^L' copy-output
-zle -N copy-last-cmd ; bindkey '^[c'  copy-last-cmd
+function copy-last-cmd () {
+  local cmd_to_copy="$BUFFER"
+
+  if ! (( ${#BUFFER//[[:space:]]} )) {
+    cmd_to_copy="$history[$(( HISTCMD - 1 ))]"
+  }
+  cmd_to_copy="${cmd_to_copy%[[:space:]]}"
+
+  echo -nE "$cmd_to_copy" | pbcopy \
+    || return $?
+
+  local -ri 10 max_len=$(( COLUMNS - ${#:-"copied: ''"} ))
+  local -r cmd="${(V)cmd_to_copy}"
+
+  if (( $#cmd <= max_len )) {
+    zle -M "copied: '$cmd'";
+  } else {
+    zle -M "copied: '${(r:max_len-3:)cmd}..."
+  }
+}
+
+# ——————————————————————————————————————————————————————————————————————————— #
+
+function copy-current-line () {
+  local -r to_copy=' | pbcopy'
+  local prefix='{ ' suffix='; }'
+
+  if ! (( ${#BUFFER//[[:space:]]} )) BUFFER="$history[$(( HISTCMD - 1 ))]"
+
+  BUFFER="${BUFFER%[[:space:]]}"
+
+  if [[ "$BUFFER" == *"$to_copy" ]] {
+    BUFFER="${BUFFER%$to_copy}"
+    return 0
+  }
+
+  local orig_buf="$BUFFER"
+
+  if [[ "$KEYS" == $'\eC'* ]] {
+    BUFFER="$BUFFER$to_copy"
+    (( CURSOR >= $#orig_buf ? ( CURSOR += $#to_copy ) : 0 ))
+
+    return 0
+  }
+
+  if [[ "$BUFFER" == *$'\n'* ]] prefix=$'\n{' suffix=$'\n}'
+
+  BUFFER="${BUFFER#[$'\t ']}"
+  orig_buf="$BUFFER"
+
+  BUFFER="$prefix$BUFFER$suffix$to_copy"
+  (( CURSOR += (CURSOR >= $#orig_buf ? $#suffix + $#to_copy : 0) + $#prefix ))
+
+  return 0
+}
+
+# ——————————————————————————————————————————————————————————————————————————— #
+
+bindkey '^[c'   copy-last-cmd   ; zle -N copy-last-cmd
+bindkey '^[C'   copy-current-line ; zle -N copy-current-line
+bindkey '^[^[C' copy-current-line
+
+# ——————————————————————————————————————————————————————————————————————————— #
+
+# spell:ignoreRegexp /\\([␛e]|0?33)\[[0-9;]*m\B/g
