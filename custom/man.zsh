6fada4bd7d495ea04a11069f4e946f1b3993768a
JV <jaiveer.chadda@gmail.com>
2026-06-26 07:52:01 +0530

    Reworked the `man` function, so that it can accept the previous line's command input, and so it doesn't mangle the buffer

--- custom/man.zsh
+++ custom/man.zsh
@@ -1,8 +1,6 @@
 #!/usr/bin/env zsh
 
 function zle-run-man () {
-  if ! (( ${#BUFFER//[[:space:]]} )) { zle beep; return; }
-
   local entry=
 
   if (( REGION_ACTIVE )) {
@@ -13,13 +11,28 @@ function zle-run-man () {
     entry="${(*)${(*)BUFFER[left+1,right]/#[[:space:]]##}/%[[:space:]]##}"
 
   } else {
-    local -a words=( "${(@z)BUFFER}" )
-    entry="$words[1]"
+    local -a words
 
+    if (( ${#BUFFER//[[:space:]]} )) {
+      words=( "${(@z)BUFFER}" )
+    } else {
+      words=( "${(@z)history[$(( HISTCMD - 1 ))]}" )
+    }
+
+    entry="$words[1]"
     if [[ "$entry" == (bundle|fish|git|gh|npm|port) ]] entry+="-$words[2]"
   }
 
-  man "$entry"
+  # go down one line so we don't overwrite the command line text
+  echo
+  zle push-line
+
+  MANWIDTH=$(( COLUMNS - 4 )) man "$entry"
+
+  # go up one line, and the to the end of the screen, so that when we
+  #  `accept-line`, it doesn't add an extra newline
+  echo -n $'\e[A\e['$COLUMNS'G'
+  zle accept-line
 }
 
 zle -N        zle-run-man
